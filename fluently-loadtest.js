a1685a03fc6c1fafe1bf7ce0ce0653445d3c4e9b
Stephen Lautier <stephen.lautier@outlook.com>
2024-07-24 18:41:00 +0200

    add content type

--- fluently-loadtest.js
+++ fluently-loadtest.js
@@ -5,6 +5,9 @@ export const options = {
 	duration: "30s",
 };
 
+// export default function () {
+// 	http.get("http://localhost:5500/api/heroes/azmodan");
+// }
 export default function () {
-	http.get("http://localhost:5500/api/heroes/azmodan");
+	http.get("http://localhost:5500/api/heroes/azmodan", { headers: { "Content-Type": "application/x-msgpack" } });
 }
